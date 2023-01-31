4150c679895959de1be4adf38263cf281d6983fe
fixurinternet <ignacio.hoffmann2010gmail.com>
2023-01-31 12:30:11 -0300

    ejercicios resueltos

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -5,6 +5,18 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	let sueldo
+	let resultado
+	let aumento
 
-	alert("ok");
+	sueldo = parseInt(document.getElementById("txtIdSueldo").value);
+	
+	resultado = sueldo * 10 / 100
+
+	aumento = resultado + sueldo
+
+	document.getElementById("txtIdResultado").value = aumento;
+	
+
+
 }
--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,5 +5,16 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
-}
+	let importe
+	let resultado
+	let importeConDescuento
+
+	importe = parseInt(document.getElementById("txtIdImporte").value);
+
+	resultado = importe * 25 / 100 
+
+	importeConDescuento = importe - resultado
+
+	document.getElementById("txtIdResultado").value = importeConDescuento;
+
+	}
